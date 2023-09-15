ff19b1917fe873d7dc4c85329911058657233f11
koralle <mkhd7930@gmail.com>
2023-09-16 00:06:18 +0900

    chore(lang): attr `lang` of <html> to `ja`

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,7 @@ export const links: LinksFunction = () => [...(cssBundleHref ? [{ rel: "styleshe
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
